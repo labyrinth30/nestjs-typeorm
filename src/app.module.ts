e509f77a464ae92aea31f9b01f7cf24017a17b96
younha00 <ayeon0@office.skhu.ac.kr>
2024-12-11 01:56:25 +0900

    Joi로 환경변수 검증하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,34 +14,71 @@ import {
 import { ProfileModel } from './entity/profile.entity';
 import { PostModel } from './entity/post.entity';
 import { TagModel } from './entity/tag.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forFeature([UserModel, ProfileModel, PostModel, TagModel]),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [
-        UserModel,
-        StudentModel,
-        TeacherModel,
-        BookModel,
-        CarModel,
-        ComputerModel,
-        AirplaneModel,
-        SingleBaseModel,
-        ProfileModel,
-        PostModel,
-        TagModel,
-      ],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('postgres').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
     }),
+    TypeOrmModule.forFeature([UserModel, ProfileModel, PostModel, TagModel]),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [
+          UserModel,
+          StudentModel,
+          TeacherModel,
+          BookModel,
+          CarModel,
+          ComputerModel,
+          AirplaneModel,
+          SingleBaseModel,
+          ProfileModel,
+          PostModel,
+          TagModel,
+        ],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: process.env.DB_TYPE as 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: [
+    //     UserModel,
+    //     StudentModel,
+    //     TeacherModel,
+    //     BookModel,
+    //     CarModel,
+    //     ComputerModel,
+    //     AirplaneModel,
+    //     SingleBaseModel,
+    //     ProfileModel,
+    //     PostModel,
+    //     TagModel,
+    //   ],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
